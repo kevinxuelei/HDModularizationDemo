865198b857828087f3bcb82cb1b4985ab9fa7e59
kevinxuelei <kevinxuelei@163.com>
2018-11-14 17:49:06 +0800

    Initial Commit

---
+++ HDModularizationDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HDModularizationDemo
+//
+//  Created by 董学雷 on 2018/11/14.
+//  Copyright © 2018 董学雷. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HDModularizationDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HDModularizationDemo
+//
+//  Created by 董学雷 on 2018/11/14.
+//  Copyright © 2018 董学雷. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
